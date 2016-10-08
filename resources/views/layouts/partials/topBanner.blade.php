dd4bf9138e70cdfa029cdd88da82bb0e31d3948e
Austin Slominski <aceslowman@Austins-MacBook-Pro.local>
2016-10-08 17:47:18 -0600

    Added social media icons

--- resources/views/layouts/partials/topBanner.blade.php
+++ resources/views/layouts/partials/topBanner.blade.php
@@ -7,8 +7,8 @@
 </div>
 
 <div class="col-md-4 hidden-sm hidden-xs banner-contact pull-right">
-	<ul>
-		<li><strong>Phone:</strong> 406.207.2708</li>
-		<li><strong>Email:</strong> austin.slominski@gmail.com</li>
-	<ul>
+	<a href=""><img class="contact-logo pull-right" src="{{ asset('img/facebook-logo.png') }}"></a>
+	<a href="https://twitter.com/aceslowman"><img class="contact-logo pull-right" src="{{ asset('img/twitter-logo.png') }}"></a>
+	<a href="https://github.com/AustinSlominski"><img class="contact-logo pull-right" src="{{ asset('img/github-logo.png') }}"></a>
+	<a href="mailto:austin.slominski@gmail.com"><img class="contact-logo pull-right" src="{{ asset('img/mail-logo.png') }}"></a>
 </div>
