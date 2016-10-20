174e1716cd9fd659d5d3ea2a2cb4f618ec4aefd5
Austin Slominski <aceslowman@Austins-MacBook-Pro.local>
2016-10-19 20:33:02 -0600

    Changes in layout made, began moving over to sass

---
+++ resources/views/notebook-entries/laravel-sass.md
@@ -0,0 +1,95 @@
+## Installing and configuring SASS for Laravel
+
+A component of Laravel is Elixir. It provides an API for gulp tasks within a Laravel project, which will allow us to compile SASS.
+
+### Installation
+
+Node.js is required, so first get that package. You can check first with `node -v` in your terminal. I prefer using Brew to install packages on my Macbook, and the command for that is:
+
+```
+brew install node
+```
+
+Now, install Gulp globally, using Node.
+
+```
+npm install --global gulp
+```
+
+And install the dependencies.
+
+```
+npm install
+```
+
+### Usage
+
+The files you need to handle are in two places. There is *gulpfile.js* located at the project root, and *resources/assets/sass*. 
+
+Gulpfile.js is where you want to point to your sass files, which by default will be your app.scss within resources/assets/sass. It's within app.scss that I usually point to all of the other scss files that I want to compile.
+
+For example, this app.scss from Mumble (which could definitely use some cleanup)
+
+```
+@import "node_modules/bootstrap-sass/assets/stylesheets/bootstrap";
+
+//GLOBAL VARIABLES
+	$hover_color: orange;
+	$toolbar_color: #333333;
+	$toolbar_hovercolor: orange;
+	$secondary_toolbar_color: #ebebeb;
+//----------------
+
+@import "mixins";
+@import "global";
+@import "dropzone";
+
+@import "glyph";
+@import "toolbar";
+@import "navigation";
+@import "tags";
+@import "photos";
+@import "forum";
+@import "index";
+@import "profile";
+@import "forum";
+@import "home";
+@import "footer";
+```
+
+And a sample from profile.scss
+
+```
+.status {
+	@extend .row, .center-block, .panel, .panel-default;
+	margin-bottom:0px;
+	div{
+		@extend .panel-body;
+		padding:0px !important;
+		h2 {
+			margin-top:0px;
+		}
+		.status-left{
+			padding:15px !important;
+			@extend .col-md-6, .col-sm-6;
+		}
+		.status-right{
+			padding:15px !important;
+			@extend .col-md-6, .col-sm-6;
+		}
+	}
+}
+
+.profile-content {
+	@extend .row, .center-block;
+	.profile-avatar {
+		@extend .pull-left, .col-md-4;
+	}
+	.profile-info {
+		@extend .pull-right, .col-md-8;
+	}
+}
+```
+
+Finally, there are a few commands you need to call from the terminal, using either `gulp` to run tasks, or `gulp watch` to detect changes and run tasks automatically.
+
--- resources/views/notebook.blade.php
+++ resources/views/notebook.blade.php
@@ -11,47 +11,64 @@
 	@stop
 	
 	@section('content')
-	<div class="content-block">
-	<div class="col-md-12">
+<div class="content-block">
+	<div class="col-md-12 row">
 		<h2>Notebook</h2>
 		<p><em>This notebook contains both my daily notes about whatever, and also my references for what I work on and learn. Some of this might be useful, some useless, I'll try to make it useful to others when I can.</em></p><hr>
 	</div>
-
+	<div class="row">
 		<div class="col-md-6">
 			<h3>Daily Notes</h3>
 			<ul>
-				<li><a href="/notebook/2016-10-04">2016-10-04</a></li>
-				<li><a href="/notebook/2016-10-07">2016-10-07</a></li>
-				<li><a href="/notebook/2016-10-10">2016-10-10</a></li>
-				<li><a href="/notebook/2016-10-11">2016-10-11</a></li>
-				<li><a href="/notebook/2016-10-12">2016-10-12</a></li>
-				<li><a href="/notebook/2016-10-19">2016-10-19</a></li>
+				<li><a href="/notebook/2016-10-04"><b>[2016-10-04]</b></a></li>
+				<li><a href="/notebook/2016-10-07"><b>[2016-10-07]</b></a></li>
+				<li><a href="/notebook/2016-10-10"><b>[2016-10-10]</b></a></li>
+				<li><a href="/notebook/2016-10-11"><b>[2016-10-11]</b></a></li>
+				<li><a href="/notebook/2016-10-12"><b>[2016-10-12]</b></a></li>
+				<li><a href="/notebook/2016-10-19"><b>[2016-10-19]</b></a></li>
 			</ul>
 		</div>
 		<div class="col-md-6">
 			<h3>openFrameworks</h3>
 			<ul>
-				<li><a href="/notebook/openframeworks-general">2016-10-04 General oF</a></li>
-				<li><a href="/notebook/openframeworks-renaming">2016-10-04 Renaming Projects</a></li>
-				<li><a href="/notebook/openframeworks-shadersetup">2016-10-07 Shader Setup</a></li>
-				<li><a href="/notebook/openframeworks-framebyframe">2016-10-07 ofVideoGrabber frame-by-frame rendering</a></li>		
-				<li><a href="/notebook/openframeworks-drawsimplesine">2016-10-19 Drawing a simple sine wave</a></li>						
+				<li><a href="/notebook/openframeworks-general">[2016-10-04]<b> General oF</b></a></li>
+				<li><a href="/notebook/openframeworks-renaming">[2016-10-04]<b> Renaming Projects</b></a></li>
+				<li><a href="/notebook/openframeworks-shadersetup">[2016-10-07]<b> Shader Setup</b></a></li>
+				<li><a href="/notebook/openframeworks-framebyframe">[2016-10-07]<b> ofVideoGrabber frame-by-frame rendering</b></a></li>		
+				<li><a href="/notebook/openframeworks-drawsimplesine">[2016-10-19]<b> Drawing a simple sine wave</b></a></li>						
 			</ul>
 		</div>
-
-
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<h3>Sound</h3>
+			<ul>
+				<li><a href="/notebook/granularsynthesis">[2016-10-19] <b>Granular Synthesis</b></a></li>
+				<li><a href="/notebook/windowfunctions">[2016-10-19] <b>Window Functions</b></a></li>
+			</ul>
+		</div>		
 		<div class="col-md-6">
 			<h3>Max/MSP</h3>
 			<ul>
-				<li><a href="/notebook/max-groove">2016-10-11 [groove~]</a></li>				
+				<li><a href="/notebook/max-groove">[2016-10-11] <b>[groove~]</b></a></li>	<li><a href="/notebook/max-jitmatrix">[2016-10-19]<b> [jit.matrix]</b></a></li>
+				<li><a href="/notebook/max-jitpeek">[2016-10-19] <b>[jit.peek]</b></a></li>	
+				<li><a href="/notebook/max-poke">[2016-10-19] <b>[poke~]</b></a></li>
 			</ul>
 		</div>
+	</div>	
+	<div class="row">	
 		<div class="col-md-6">
 			<h3>GLSL</h3>
 			<ul>
-				<li><a href="/notebook/glsl-heatdistortion">2016-10-13 Creating a heat distortion effect</a></li>	
+				<li><a href="/notebook/glsl-heatdistortion">[2016-10-13]<b> Creating a heat distortion effect</b></a></li>	
 			</ul>
 		</div>
-
-	</div>
+		<div class="col-md-6">
+			<h3>Web</h3>
+			<ul>
+				<li><a href="/notebook/laravel-sass">[2016-10-19]<b> Installing and configuring SASS for Laravel</b></a></li>	
+			</ul>
+		</div>
+	</div>		
+</div>
 	@stop
